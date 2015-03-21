9f006f0290c37be551ca8cf4a4f9dba3dfde734c
Lark-wrc <willowlark@outlook.com>
2015-03-21 15:24:01 -0400

    Comments added

--- Item.rb
+++ Item.rb
@@ -1,13 +1,22 @@
+#A Interface for all items in the game. Every item inherits from here.
 class Item 
 	attr_accessor :name,uses,worth,iconID,owner
+	
+	#Initializes properities of an item. 
 	def initialize(uses, worth, name, iconID)
 		@uses = uses
 		@worth = worth
 		@name = name
 		@iconID = iconID
 		@owner - ""
+	
+	#If this item does something when the use option is selected, this will
+	#do that. Takes a unit as items tend to affect the unit using it.
 	def action(unit)
 	end
+	
+	#Asks if this item can be used by a particular unit. Same logic as not asking
+	#a sword if it is a sword.
 	def ask_usable(unit)
 	end
 end
--- Weapon.rb
+++ Weapon.rb
@@ -1,6 +1,12 @@
 include Item
+#This is a interface for all weapons. It contains the methods every weapon
+#will need. It is a subclass of Item.
 class Weapon < Item
 	attr_accessor :name,uses,worth,iconID,owner,rank,mt,hit,crit,range,effective
+	
+	#Creates the properties of a weapon. Used to create any new weapon object.
+	#Type may seem like it is missing, but do remember this is an interface. Type is
+	#Dictated by the class. This allows for new types of weapons to be created.
 	def initialize(rank, mt, hit, crit, range, effective, uses, worth, name, iconID)
 		super(uses, worth, name, iconID)
 		@rank = rank
@@ -9,18 +15,27 @@ class Weapon < Item
 		@crit = crit
 		@range = range
 		@effective = effective
+	
+	#Instructions to be run when a weapon is set to a unit's equipped slot. 
 	def on_equip(unit)
 	end
 	
+	#If this weapon does anything special, such as allow an extra attack or grant
+	#a combat skill to it's user, this method will apply it to the stack provided.
 	def combat_action(stack)
 	end
 	
+	#The inverse of on_equip.
 	def off_equip(unit)
 	end
 	
+	#Returns true or false if a unit can equip this weapon. Will check itself against the
+	#provided unit's class's equippible list. 
 	def equippible?(unit)
 	end
 	
+	#Returns the stat (magic or strength) to be used in the combat calculator when attacking
+	#with this weapon. Allows for modularity and follows the logic of a sword knowing it's a sword.
 	def relevantstat?()
 	end
 end
