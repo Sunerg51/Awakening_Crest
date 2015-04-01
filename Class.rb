717f7b16385de4baf4d494421c3dd423006288ef
Lark-wrc <willowlark@outlook.com>
2015-04-01 14:26:28 -0400

    same bugs diff commit

--- Class.rb
+++ Class.rb
@@ -1,6 +1,6 @@
 #An interface for all class(job) types that a unit can have
 class Class
-	attr_accessor :maxClassStat,currentLv,exp,promote,lvCap,promoteLv, promotions, name, profics
+	attr_accessor :maxClassStat,:currentLv,:exp,:promote,:lvCap,:promoteLv,:promotions,:name,:profics
 	
 	#Initialize class properties
 	def initialize(maxClassStat,currentLv,exp,promote,lvCap,promoteLv, promotions, name, profics)
--- WeaponList.rb
+++ WeaponList.rb
@@ -1,6 +1,6 @@
 #Class that holds proficiencies for each weapon as ints
 def WeaponList	
-	attr_accessor :sword, lance, axe, bow, anima, dark, light, staff
+	attr_accessor :sword,:lance,:axe,:bow,:anima,:dark,:light,:staff
 	
 	#Initialize WeaponList properties
 	def initialize(sword, lance, axe, bow, anima, dark, light, staff)
